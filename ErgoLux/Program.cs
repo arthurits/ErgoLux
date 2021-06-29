64d6abe9ae78cfc6854b3d7763d1f30e63058c17
—ins— <insht_cnct@hotmail.es>
2021-06-29 12:13:47 +0200

    Code updated

--- ErgoLux/Program.cs
+++ ErgoLux/Program.cs
@@ -17,7 +17,7 @@ namespace ErgoLux
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmTest());
+            Application.Run(new FrmMain());
         }
     }
 }
