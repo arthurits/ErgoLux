3ddd09dbc2e3acf1a1f124ed6c35e0cc6a23c963
—ins— <insht_cnct@hotmail.es>
2022-02-02 12:52:06 +0100

    Update data files

--- ErgoLux/Form2.cs
+++ ErgoLux/Form2.cs
@@ -1,26 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿namespace ErgoLux;
 
-namespace ErgoLux
+public partial class Form2 : Form
 {
-    public partial class Form2 : Form
+    public Form2()
     {
-        public Form2()
-        {
-            InitializeComponent();
+        InitializeComponent();
 
-            double[] dataX = new double[] { 1, 2, 3, 4, 5 };
-            double[] dataY = new double[] { 1, 4, 9, 16, 25 };
-            formsPlot1.Plot.AddScatter(dataX, dataY);
-            formsPlot1.Refresh();
-        }
-
+        double[] dataX = new double[] { 1, 2, 3, 4, 5 };
+        double[] dataY = new double[] { 1, 4, 9, 16, 25 };
+        formsPlot1.Plot.AddScatter(dataX, dataY);
+        formsPlot1.Refresh();
     }
+
 }
+
