c1fc8a9e209f794ea0d77169f4f6167fd3a711df
—ins— <insht_cnct@hotmail.es>
2022-02-14 14:26:48 +0100

    Update settings

--- ErgoLux/Settings.cs
+++ ErgoLux/Settings.cs
@@ -1,62 +1,57 @@
-﻿using System;
-using System.Drawing;
-using System.IO;
-using System.Text;
+﻿using System.Drawing;
 using System.Text.Json;
-using System.Windows.Forms;
 
-namespace ErgoLux
+namespace ErgoLux;
+
+partial class FrmMain
 {
-    partial class FrmMain
+    /// <summary>
+    /// Loads all settings from file _sett.FileName into class instance _sett
+    /// Shows MessageBox error if unsuccessful
+    /// </summary>
+    private void LoadProgramSettingsJSON()
     {
-        /// <summary>
-        /// Loads all settings from file _sett.FileName into class instance _sett
-        /// Shows MessageBox error if unsuccessful
-        /// </summary>
-        private void LoadProgramSettingsJSON()
+        try
         {
-            try
-            {
-                var jsonString = File.ReadAllText(_sett.SettingsFileName);
-                _sett = JsonSerializer.Deserialize<ClassSettings>(jsonString);
-                _sett.InitializeJsonIgnore(_sett.AppPath);
+            var jsonString = File.ReadAllText(_sett.SettingsFileName);
+            _sett = JsonSerializer.Deserialize<ClassSettings>(jsonString);
+            _sett.InitializeJsonIgnore(_sett.AppPath);
 
-                this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-                this.DesktopLocation = new Point(_sett.Wnd_Left, _sett.Wnd_Top);
-                this.ClientSize = new Size(_sett.Wnd_Width, _sett.Wnd_Height);
-            }
-            catch (FileNotFoundException)
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.DesktopLocation = new Point(_sett.Wnd_Left, _sett.Wnd_Top);
+            this.ClientSize = new Size(_sett.Wnd_Width, _sett.Wnd_Height);
+        }
+        catch (FileNotFoundException)
+        {
+        }
+        catch (Exception ex)
+        {
+            using (new CenterWinDialog(this))
             {
-            }
-            catch (Exception ex)
-            {
-                using (new CenterWinDialog(this))
-                {
-                    MessageBox.Show(this,
-                        StringsRM.GetString("strMsgBoxErrorSettings", _sett.AppCulture) ?? $"Error loading settings file.\n\n{ex.Message}\n\nDefault values will be used instead.",
-                        StringsRM.GetString("strMsgBoxErrorSettingsTitle", _sett.AppCulture) ?? "Error",
-                        MessageBoxButtons.OK,
-                        MessageBoxIcon.Error);
-                }
+                MessageBox.Show(this,
+                    StringsRM.GetString("strMsgBoxErrorSettings", _sett.AppCulture) ?? $"Error loading settings file.\n\n{ex.Message}\n\nDefault values will be used instead.",
+                    StringsRM.GetString("strMsgBoxErrorSettingsTitle", _sett.AppCulture) ?? "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
             }
         }
+    }
 
-        /// <summary>
-        /// Saves data from class instance _sett into _sett.FileName
-        /// </summary>
-        private void SaveProgramSettingsJSON()
+    /// <summary>
+    /// Saves data from class instance _sett into _sett.FileName
+    /// </summary>
+    private void SaveProgramSettingsJSON()
+    {
+        _sett.Wnd_Left = DesktopLocation.X;
+        _sett.Wnd_Top = DesktopLocation.Y;
+        _sett.Wnd_Width = ClientSize.Width;
+        _sett.Wnd_Height = ClientSize.Height;
+
+        var options = new JsonSerializerOptions
         {
-            _sett.Wnd_Left = DesktopLocation.X;
-            _sett.Wnd_Top = DesktopLocation.Y;
-            _sett.Wnd_Width = ClientSize.Width;
-            _sett.Wnd_Height = ClientSize.Height;
-
-            var options = new JsonSerializerOptions
-            {
-                WriteIndented = true
-            };
-            var jsonString = JsonSerializer.Serialize(_sett, options);
-            File.WriteAllText(_sett.SettingsFileName, jsonString);
-        }
+            WriteIndented = true
+        };
+        var jsonString = JsonSerializer.Serialize(_sett, options);
+        File.WriteAllText(_sett.SettingsFileName, jsonString);
     }
 }
